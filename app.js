0335c2253ec2cdafc72fd6d5e80647d983aa364a
jkaplanis <johntkaplanis@gmail.com>
2020-04-14 19:23:57 -0700

    add function for adding new department and viewing new department

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -25,16 +26,30 @@ const start = () => {
     .prompt({
       name: "action",
       type: "list",
-      message: "Would you like to [Add], [View], or [Update] a department?",
-      choices: ["Add", "View", "Update"]
+      message: "What would you like to do?",
+      choices: [
+        "Add departments",
+        "Add roles",
+        "Add employees",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee roles"
+      ]
     })
     .then(answer => {
       // based on their answer, call appropriate function
-      if (answer.action === "Add") {
-        return add();
-      } else if (answer.action === "View") {
+      if (answer.action === "Add departments") {
+        return addDepartments();
+      } else if (answer.action === "Add roles") {
         return view();
-      } else if (answer.action === "Update") {
+      } else if (answer.action === "Add employees") {
+        return update();
+      } else if (answer.action === "View departments") {
+        return viewDepartments();
+      } else if (answer.action === "View roles") {
+        return update();
+      } else if (answer.action === "Update employee roles") {
         return update();
       } else {
         connection.end();
@@ -46,5 +61,40 @@ const start = () => {
     });
 };
 
-// add department first
-const add = () => {};
+// add department
+const addDepartments = () => {
+  return inquirer
+    .prompt({
+      name: "departmentName",
+      type: "input",
+      message: "What is the name of the department?"
+    })
+    .then(answer => {
+      return connection.query(
+        "INSERT INTO department SET ?",
+        {
+          department_name: answer.departmentName
+        },
+        err => {
+          if (err) {
+            throw err;
+          }
+          console.log("Your department was added successfully!");
+          // re-prompt the user for their next action
+          return start();
+        }
+      );
+    });
+};
+
+// view department
+const viewDepartments = () => {
+  return connection.query("SELECT * FROM department", (err, results) => {
+    if (err) {
+      throw err;
+    }
+    const table = cTable.getTable(results);
+    console.log(table);
+    return start();
+  });
+};
--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   department_name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,2) NULL,
   department_id INT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
